1888cd7e7352e9c4363cd91a9953e4b7c936be22
Celdama <celdama.dev@gmail.com>
2022-01-26 10:34:52 +0100

    GlobalStyle: update variable

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,18 +2,23 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root{
+    --maxWidth: 1800px;
     --black: #232025;
-    --rose: #d5cdc4
+    --rose: #d5cdc4;
+    --fontSuperBig: 2.5rem;
+    --fontBig: 1.5rem;
+    --fontMed: 1.2rem;
+    --fontSmall: 1rem;
   }
 
   * {
     box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+    font-family: sans-serif;
   }
 
   body {
-    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
     background-color: var(--black);
     color: var(--rose);
   }
