cadc6b948e07766dbe071c4b7722659070262ac8
Celdama <celdama.dev@gmail.com>
2022-01-30 07:45:15 +0100

    Components/ItemList/index: import custom hooks for motion effect

--- src/Components/ItemList/index.js
+++ src/Components/ItemList/index.js
@@ -1,22 +1,13 @@
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React from 'react';
+import { motion } from 'framer-motion';
 import { Wrapper, Content } from './itemList.style';
+import { useMotionEffectWithChildren } from '../../hooks/useMotionEffectWithChildren';
 import Item from '../Item';
 
 const ItemList = () => {
+  const [controls, ref] = useMotionEffectWithChildren();
+
   const items = ['mango', 'banana', 'pineapple', 'pitahaya', 'variety'];
-
-  const controls = useAnimation();
-  const { ref, inView } = useInView();
-
-  useEffect(() => {
-    if (inView) {
-      controls.start('visible');
-    } else {
-      controls.start('hidden');
-    }
-  }, [controls, inView]);
 
   const container = {
     hidden: { opacity: 0 },
@@ -29,7 +20,7 @@ const ItemList = () => {
     },
   };
 
-  const itemLi = {
+  const children = {
     hidden: {
       y: 0,
       opacity: 0,
@@ -38,15 +29,15 @@ const ItemList = () => {
       y: 5,
       opacity: 1,
       transition: {
-        type: 'inertia',
-        velocity: 20,
+        transition: 'backInOut',
+        duration: 1,
       },
     },
   };
 
   const content = items.map((item, index) => {
     return (
-      <motion.li variants={itemLi}>
+      <motion.li variants={children}>
         <Item key={index} name={item} id={index + 1} />
       </motion.li>
     );
