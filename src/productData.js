0df342ab3bc254a863c52804d016674acc62a0bc
Celdama <celdama.dev@gmail.com>
2022-01-30 08:34:27 +0100

    src: added product data

---
+++ src/productData.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    name: 'mango',
+    price: 29.0,
+    description:
+      'The OG tala flavor, ripe mangoes from asia frozen in time their ripest. Crunchy, vegan, just guilt free snacking. 12 packs',
+    ingredients: 'mango',
+    mainImg: './Images/products/mango/mainImg.jpg',
+  },
+  {
+    name: 'banana',
+    price: 29.0,
+    description:
+      "Bursting with flavor, this is the banana you've always wanted. Crunchy, vegan, just guilt free snacking. 12 PACKS.",
+    ingredients: 'banana',
+    mainImg: './Images/products/banana/mainImg.jpg',
+  },
+  {
+    name: 'banana',
+    price: 29.0,
+    description:
+      'A little tart, a little sweet, a lot pineapple. Crunchy, vegan, just guilt free snacking. 12 PACKS.',
+    ingredients: 'banana',
+    mainImg: './Images/products/pineapple/mainImg.jpg',
+  },
+  {
+    name: 'pitahaya',
+    price: 29.0,
+    description:
+      'Is it pitahaya, pitaya or dragon fruit? Who cares it tastes good. Crunchy, vegan, just guilt free snacking. 12 PACKS.',
+    ingredients: 'pitahaya',
+    mainImg: './Images/products/pitahaya/mainImg.jpg',
+  },
+  {
+    name: 'Variety',
+    price: 29.0,
+    description:
+      "Can't decide? Don't blame you. Enjoy our three most popular crunchy, vegan, guilt free flavors. 12 PACKS. ",
+    ingredients: 'Mango, Banana, Pineapple, Pitahaya',
+    mainImg: './Images/products/pitahaya/mainImg.jpg',
+  },
+];
