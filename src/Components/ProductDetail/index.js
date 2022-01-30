217b70945946021bf716f6d5a5a46c16e7f9b9b2
Celdama <celdama.dev@gmail.com>
2022-01-30 19:47:16 +0100

    Components/ProductDetail: add style

--- src/Components/ProductDetail/index.js
+++ src/Components/ProductDetail/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { products } from '../../productData';
 
-import { Image } from './productDetail.style';
+import { Wrapper, Content, Image, Item } from './productDetail.style';
 import getCoverProductByKey from '../../Helpers/getCoverProductByKey';
 
 const ProductDetail = () => {
@@ -11,33 +11,66 @@ const ProductDetail = () => {
   const [otherProducts, setOtherProducts] = useState([]);
   const [currentProductImage, setCurrentProductImage] = useState([]);
   const [productImages, setProductImages] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
     products.forEach((product) => {
       if (product.id === id) {
         setCurrentProduct(product);
-        setProductImages(getCoverProductByKey(product.name));
-        setCurrentProductImage(getCoverProductByKey(product.name)[0]);
-      } else {
-        setOtherProducts((prevState) => [...prevState, product]);
+        const productImages = getCoverProductByKey(product.name);
+        setCurrentProductImage(productImages[0]);
+        setProductImages(productImages);
       }
+      // else {
+      //   setOtherProducts((prevState) => [...prevState, product]);
+      // }
+
+      setAllProducts((prevState) => [...prevState, product]);
     });
   }, []);
 
-  return (
-    <div>
-      <div>
-        <h3>{currentProduct.name}</h3>
-        <p>${currentProduct.price}</p>
-        <p>{currentProduct.description}</p>
-        <Image src={currentProductImage} />
+  console.log(allProducts);
 
-        {productImages.map((src) => {
-          return <img src={src} alt='#' />;
-        })}
-      </div>
-    </div>
+  return (
+    <Wrapper>
+      <Content>
+        <div className='left'>
+          <Image src={currentProductImage} />
+        </div>
+        <div className='right'>
+          <div className='product-list'>
+            <ul>
+              {allProducts.map((product) => {
+                const isCurrent = product.name === currentProduct.name;
+                // au click sur un produit ici, mettre à jour le currentProduct grâce à l'id !
+                return <Item current={isCurrent}>{product.name}</Item>;
+              })}
+            </ul>
+          </div>
+
+          <div className='product-info'>
+            <div className='product-text'>
+              <h3 className='product-name'>{currentProduct.name}</h3>
+              <p className='product-price'>$ {currentProduct.price}.00 USD</p>
+              <p className='product-desc'>{currentProduct.description}</p>
+              <p className='product-marketing'>
+                can't decide which flavor is right for you ? <br /> try our
+                sample pack
+              </p>
+              <button>add to card</button>
+            </div>
+            <div className='product-photos'>
+              {productImages.map((src) => {
+                return <img className='thumbnail' src={src} alt='#' />;
+              })}
+            </div>
+          </div>
+        </div>
+      </Content>
+    </Wrapper>
   );
 };
+
+/*  */
 
 export default ProductDetail;
--- src/Components/ProductDetail/productDetail.style.js
+++ src/Components/ProductDetail/productDetail.style.js
@@ -1,9 +1,116 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.section`
+  padding: 80px 20px;
+`;
+
+export const Content = styled.div`
+  position: relative;
+  /* padding: 20px 0; */
+  width: 100%;
+  /* outline: 1px solid yellow; */
+  display: flex;
+
+  .left {
+    /* outline: 1px solid blue; */
+  }
+
+  .right {
+    /* outline: 1px solid green; */
+    /* padding: 160px 0; */
+    display: flex;
+    flex-direction: column;
+
+    justify-content: flex-end;
+    align-items: flex-end;
+
+    .product-list {
+      /* outline: 1px solid red; */
+      width: 100%;
+      padding: 16px 30px;
+
+      ul {
+        text-transform: uppercase;
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+        list-style: none;
+      }
+    }
+
+    .product-info {
+      /* outline: 1px solid red; */
+      padding: 0 30px;
+      height: 58%;
+
+      display: flex;
+
+      .product-text {
+        width: 70%;
+        /* outline: 1px solid white; */
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: flex-start;
+
+        .product-desc {
+          width: 60%;
+        }
+
+        button {
+          width: 280px;
+          height: 64px;
+          background-color: transparent;
+          border-radius: 32px;
+          font-size: 12px;
+          text-transform: uppercase;
+          border: 1px solid var(--rose);
+          color: var(--rose);
+        }
+      }
+
+      .product-photos {
+        /* outline: 1px solid red; */
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .thumbnail {
+          height: 43px;
+          width: 72px;
+        }
+      }
+    }
+
+    h3 {
+      font-size: 15vw;
+      letter-spacing: 22px;
+      position: absolute;
+      text-transform: uppercase;
+      top: 40px;
+      text-align: right;
+      left: 0;
+      /* outline: 1px solid violet; */
+      width: 100%;
+      margin: 0;
+      padding: 0 30px;
+      text-shadow: -1px 0 0 #d5cdc4, 0 1px 0 #d5cdc4, 1px 0 0 #d5cdc4,
+        0 -1px 0 #d5cdc4;
+      color: #232025;
+    }
+  }
+`;
+
 export const Image = styled.img`
-  height: 1000px;
-  width: 700px;
+  height: 733px;
+  width: 639px;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat; ;;;
+`;
+
+export const Item = styled.li`
+  border-bottom: ${({ current }) => (current ? '2px solid yellow' : '')};
 `;
