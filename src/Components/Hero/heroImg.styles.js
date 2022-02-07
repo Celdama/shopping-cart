d0a449a1eebe9dc182bed6130961ad1203b72080
Celdama <celdama.dev@gmail.com>
2022-02-07 20:37:09 +0100

    Components/Hero/heroImg.styles: refactoring

--- src/Components/Hero/heroImg.styles.js
+++ src/Components/Hero/heroImg.styles.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
+import breakpoints from '../../Helpers/breakpoints';
 
 export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   background: url(${({ image }) => image});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   width: 100%;
   height: 600px;
-  display: flex;
-  justify-content: space-between;
   overflow-x: hidden;
 
-  @media screen and (min-width: 992px) {
+  @media screen and (min-width: ${breakpoints.md}) {
     height: 110vh;
   }
 
@@ -19,11 +20,11 @@ export const Wrapper = styled.div`
     background-color: var(--black);
     width: 26px;
 
-    @media screen and (min-width: 992px) {
+    @media screen and (min-width: ${breakpoints.md}) {
       width: 102px;
     }
 
-    @media screen and (min-width: 1600px) {
+    @media screen and (min-width: ${breakpoints.xl}) {
       width: 142px;
     }
   }
