54f9e5f6fbc8fc7e97e35407ed84ba67e8909e2a
Celdama <celdama.dev@gmail.com>
2022-02-07 20:30:21 +0100

    Components/CheckoutSummary/checkoutSummary.style: refactoring

--- src/Components/CheckoutSummary/checkoutSummary.style.js
+++ src/Components/CheckoutSummary/checkoutSummary.style.js
@@ -2,45 +2,46 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  padding: 10px;
+  padding: 1rem;
 `;
 
 export const Content = styled.div`
   .summary-section {
-    background-color: #fff;
-    margin: 20px 0;
+    background-color: var(--white);
+    margin: 2rem 0;
     border: 1px solid lightgray;
   }
 
   .summary-header {
     display: flex;
-    padding: 0 12px;
     align-items: center;
     justify-content: space-between;
+    padding: 0 1.2rem;
     border-bottom: 1px solid lightgray;
   }
 
   .summary-body {
-    padding: 8px 12px;
+    padding: 0.8rem 1.2rem;
 
     > div {
       display: flex;
       justify-content: space-between;
-      margin: 12px 0;
+      margin: 1.2rem 0;
     }
 
     > div:last-child {
-      margin-top: 22px;
+      margin-top: 2.2rem;
       font-weight: bold;
     }
   }
+
   button {
-    background-color: #3898ec;
+    background-color: var(--inputFocus);
     border: none;
     border-radius: 3px;
-    padding: 9px 15px;
+    padding: 0.9rem 1.5rem;
     width: 100%;
-    color: #fff;
+    color: var(--white);
     cursor: pointer;
   }
 `;
