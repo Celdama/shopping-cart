cccb035e170d711a812134a5005ae77b415c06ac
Celdama <celdama.dev@gmail.com>
2022-02-07 20:11:06 +0100

    Components/CartItem/cartItem.style: refactoring

--- src/Components/CartItem/cartItem.style.js
+++ src/Components/CartItem/cartItem.style.js
@@ -19,12 +19,12 @@ export const ContentProductInfo = styled.div`
   flex-direction: column;
   justify-content: space-around;
   flex: 1 1 auto;
-  padding: 0 10px;
-  font-size: 12px;
+  padding: 0 1rem;
+  font-size: 1.2rem;
   font-weight: 600;
 
   .name {
-    font-size: 16px;
+    font-size: 1.6rem;
     font-weight: bold;
   }
 
@@ -36,16 +36,16 @@ export const ContentProductInfo = styled.div`
 `;
 
 export const ContentQuantity = styled.div`
-  width: 54px;
-  height: 32px;
-  background-color: #d5cdc4;
-  font-weight: 600;
-  font-size: 12px;
-  border-radius: 4px;
-  color: var(--black);
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 54px;
+  height: 32px;
+  background-color: var(--rose);
+  font-weight: 600;
+  font-size: 1.2rem;
+  border-radius: 4px;
+  color: var(--black);
 
   .decremente {
     border-top-left-radius: 4px;
@@ -59,13 +59,13 @@ export const ContentQuantity = styled.div`
 `;
 
 export const QuantityBtn = styled.button`
-  height: 100%;
   display: flex;
-  width: 14px;
   justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 14px;
   background-color: var(--black);
   color: var(--rose);
-  align-items: center;
   cursor: pointer;
   border: none;
 `;
