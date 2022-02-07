67f0858157579314de14945e3fec4ee55eccdd16
Celdama <celdama.dev@gmail.com>
2022-02-07 20:35:57 +0100

    Components/Divider/divider.style: refactoring

--- src/Components/Divider/divider.style.js
+++ src/Components/Divider/divider.style.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import breakpoints from '../../Helpers/breakpoints';
 
 export const Wrapper = styled.div`
   height: 20px;
-  padding: 0 20px;
+  padding: 0 2rem;
 `;
 
 export const Content = styled.div`
@@ -10,13 +11,13 @@ export const Content = styled.div`
   margin: 0 auto;
   max-width: var(--maxWidth);
 
-  @media screen and (min-width: 992px) {
+  @media screen and (min-width: ${breakpoints.md}) {
     max-width: 90%;
   }
 
   h6 {
     text-transform: uppercase;
-    margin: 6px 0;
+    margin: 0.6rem 0;
   }
 
   .divider {
