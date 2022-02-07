4cea565da8e629b757b2b7a743d809af9e0be1f5
Celdama <celdama.dev@gmail.com>
2022-02-07 20:01:09 +0100

    Components/CartCheckout/cartCheckout.style: refactoring

--- src/Components/CartCheckout/cartCheckout.style.js
+++ src/Components/CartCheckout/cartCheckout.style.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   border-top: 1px dotted var(--black);
-  padding-bottom: 20px;
+  padding-bottom: 2rem;
   text-transform: uppercase;
-  font-size: 14px;
+  font-size: 1.4rem;
 `;
 
 export const Content = styled.div`
@@ -15,22 +15,22 @@ export const Content = styled.div`
 `;
 
 export const PriceInfo = styled.div`
-  width: 100%;
-  padding: 18px 24px;
   display: flex;
   justify-content: space-between;
+  width: 100%;
+  padding: 1.8rem 2.4rem;
   font-weight: bold;
 `;
 
 export const Button = styled.button`
   height: 50px;
   width: 272px;
-  border: 1px solid #232025;
+  border: 1px solid var(--black);
   border-radius: 32px;
-  background-color: #232025;
+  background-color: var(--black);
   color: var(--rose);
   text-transform: uppercase;
   cursor: pointer;
-  font-size: 14px;
+  font-size: 1.4rem;
   font-weight: bold;
 `;
