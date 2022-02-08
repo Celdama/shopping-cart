001fa5cc12745aab4ae407366dbde38272b1240e
Celdama <celdama.dev@gmail.com>
2022-02-08 08:19:38 +0100

    Components/ProductImgSmallScreen/productImgSmallScreen.style: refactoring

--- src/Components/ProductImgSmallScreen/productImgSmallScreen.style.js
+++ src/Components/ProductImgSmallScreen/productImgSmallScreen.style.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import breakpoints from '../../Helpers/breakpoints';
 
 export const Wrapper = styled.div`
-  padding: 6px 0;
+  padding: 0.6rem 0;
   display: block;
 
-  @media screen and (min-width: 766px) {
+  @media screen and (min-width: ${breakpoints.sm}) {
     display: none;
   }
 `;
@@ -12,12 +13,12 @@ export const Wrapper = styled.div`
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 6px;
+  gap: 0.6rem;
 
   .controller {
     height: 4px;
     flex: 1 1 auto;
-    background-color: #d5cdc4;
+    background-color: var(--rose);
     cursor: pointer;
   }
 `;
