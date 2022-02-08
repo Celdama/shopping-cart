912f78ca90841cc4d127ed583e8ca9ebf7d8d1a8
Celdama <celdama.dev@gmail.com>
2022-02-08 08:21:17 +0100

    Components/ProductDetailTitle/productDetailTitle.style: refactoring

--- src/Components/ProductDetailTitle/productDetailTitle.style.js
+++ src/Components/ProductDetailTitle/productDetailTitle.style.js
@@ -1,23 +1,24 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import breakpoints from '../../Helpers/breakpoints';
 
 export const Title = styled(motion.h3)`
   font-size: 14vw;
-  letter-spacing: 22px;
+  letter-spacing: 2.2rem;
   position: absolute;
   text-transform: uppercase;
-  top: 30px;
+  top: 3rem;
   text-align: right;
   left: 0;
   width: 100%;
   margin: 0;
-  padding: 0 30px;
-  text-shadow: -1px 0 0 #d5cdc4, 0 1px 0 #d5cdc4, 1px 0 0 #d5cdc4,
-    0 -1px 0 #d5cdc4;
-  color: #232025;
+  padding: 0 3rem;
+  text-shadow: -1px 0 0 var(--rose), 0 1px 0 var(--rose), 1px 0 0 var(--rose),
+    0 -1px 0 var(--rose);
+  color: var(--black);
   display: none;
 
-  @media screen and (min-width: 766px) {
+  @media screen and (min-width: ${breakpoints.sm}) {
     display: block;
   }
 `;
