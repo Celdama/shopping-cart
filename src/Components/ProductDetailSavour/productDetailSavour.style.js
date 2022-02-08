daca41455a7c459e0e5d1add47d6028bb5fa407c
Celdama <celdama.dev@gmail.com>
2022-02-08 08:05:53 +0100

    Components/ProductDetailSavour/productDetailSavour.style: refactoring

--- src/Components/ProductDetailSavour/productDetailSavour.style.js
+++ src/Components/ProductDetailSavour/productDetailSavour.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const Item = styled(motion.li)`
-  padding: 16px 0;
+  padding: 1.6rem 1rem;
+  font-size: 1.2rem;
   cursor: pointer;
 `;
