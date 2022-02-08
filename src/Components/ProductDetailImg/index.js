c3044b47e886e5e2e5c50f9de2f49a275a147d48
Celdama <celdama.dev@gmail.com>
2022-02-08 08:18:12 +0100

    Components/ProductDetailImg: refactoring (style, content)

--- src/Components/ProductDetailImg/index.js
+++ src/Components/ProductDetailImg/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Wrapper } from './productDetailImg.style';
-import { motion, AnimatePresence } from 'framer-motion';
 import { getMainImgProductShopPageEffect } from '../../Helpers/shopPageMotionEffect';
+import { Wrapper, Image } from './productDetailImg.style';
 
 const ProductDetailImg = ({ currentProductImage }) => {
   const { img } = currentProductImage;
 
   return (
     <Wrapper>
-      <AnimatePresence>
-        <motion.img
-          className='product-img-xl'
-          key={img}
-          src={img}
-          variants={getMainImgProductShopPageEffect()}
-          initial='hidden'
-          animate='visible'
-          transition={{
-            x: { type: 'spring', stiffness: 300, damping: 30 },
-            opacity: { duration: 0.8 },
-          }}
-        />
-      </AnimatePresence>
+      <Image
+        className='product-img-xl'
+        key={img}
+        src={img}
+        variants={getMainImgProductShopPageEffect()}
+        initial='hidden'
+        animate='visible'
+        transition={{
+          x: { type: 'spring', stiffness: 300, damping: 30 },
+          opacity: { duration: 0.8 },
+        }}
+      />
     </Wrapper>
   );
 };
--- src/Components/ProductDetailImg/productDetailImg.style.js
+++ src/Components/ProductDetailImg/productDetailImg.style.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import breakpoints from '../../Helpers/breakpoints';
 
 export const Wrapper = styled.div`
   width: 100%;
   height: 420px;
 
-  @media screen and (min-width: 766px) {
+  @media screen and (min-width: ${breakpoints.sm}) {
     width: 50%;
     height: 90vh;
   }
+`;
 
-  img {
-    height: 100%;
-    width: 100%;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+export const Image = styled(motion.img)`
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 
-    animation: fadIn 0.5;
-
-    @media screen and (min-width: 766px) {
-      min-height: 640px;
-    }
+  @media screen and (min-width: ${breakpoints.sm}) {
+    min-height: 640px;
   }
 `;
