3033bb5623143a099e6b5a97202f16868d0c2409
Celdama <celdama.dev@gmail.com>
2022-02-11 19:37:26 +0100

    create redux store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,1 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { productsReducer } from './reducers/productsReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  combineReducers({
+    products: productsReducer,
+    fiter: () => console.log('filter'),
+  }),
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
