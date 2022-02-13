9ed3082f8c517449c27ef8dc91b64e9b95620bb0
Celdama <celdama.dev@gmail.com>
2022-02-13 11:32:50 +0100

    create currentProduct reducer

---
+++ src/Store/reducers/currentProductReducer.js
@@ -0,0 +1,10 @@
+export const SET_CURRENT_PRODUCT = 'SET_CURRENT_PRODUCT';
+
+export const currentProductReducer = (state = null, action) => {
+  switch (action.type) {
+    case SET_CURRENT_PRODUCT:
+      return action.payload;
+    default:
+      return state;
+  }
+};
