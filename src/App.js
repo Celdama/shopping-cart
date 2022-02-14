88aad7693604fc86602c2ca478e24fc928c3b732
Celdama <celdama.dev@gmail.com>
2022-02-14 11:37:58 +0100

    moove function to get number of product in cart from App component to NavBar component using Redux products store

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import { nanoid } from 'nanoid';
 import Menu from './Components/Menu';
-import NavBar from './Components/Navbar';
+import { NavBarStore } from './Components/Navbar';
 import Home from './Components/Home';
 import ScrollToTop from './Components/ScrollToTop';
 import { ProductDetailStore } from './Components/ProductDetail';
@@ -13,57 +13,8 @@ import { CheckoutStore } from './Components/Checkout';
 export const App = () => {
   const [displayMenu, setDisplayMenu] = useState(false);
   const [displayCart, setDisplayCart] = useState(false);
-  const [numberOfCartItems, setNumberOfCartItems] = useState(0);
-  const [cartItems, setCartItems] = useState([
-    {
-      id: nanoid(),
-      name: 'mango',
-      quantity: 0,
-      price: 29,
-      img: './Images/products/mango/mainImg.jpg',
-    },
-    {
-      id: nanoid(),
-      name: 'banana',
-      quantity: 0,
-      price: 29,
-      img: './Images/products/banana/mainImg.jpg',
-    },
-    {
-      id: nanoid(),
-      name: 'pineapple',
-      quantity: 0,
-      price: 29,
-      img: './Images/products/pineapple/mainImg.jpg',
-    },
-    {
-      id: nanoid(),
-      name: 'pitahaya',
-      quantity: 0,
-      price: 29,
-      img: './Images/products/pitahaya/mainImg.jpg',
-    },
-    {
-      id: nanoid(),
-      name: 'variety',
-      quantity: 0,
-      price: 29,
-      img: './Images/products/variety/mainImg.jpg',
-    },
-  ]);
+
   // ENREGISTRER LE CART DANS LOCALSTORAGE
-
-  useEffect(() => {
-    const handleNumberOfProduct = () => {
-      let sum = 0;
-      cartItems.forEach(({ quantity }) => {
-        sum += quantity;
-      });
-      setNumberOfCartItems(sum);
-    };
-
-    handleNumberOfProduct(cartItems);
-  }, [cartItems]);
 
   const handleDisplayCart = () => {
     setDisplayCart((prevState) => !prevState);
@@ -77,8 +28,7 @@ export const App = () => {
     <div className='App'>
       <BrowserRouter>
         <Menu displayMenu={displayMenu} handleDisplayMenu={handleDisplayMenu} />
-        <NavBar
-          numberOfCartItems={numberOfCartItems}
+        <NavBarStore
           handleDisplayCart={handleDisplayCart}
           handleDisplayMenu={handleDisplayMenu}
         />
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper, Content } from './navbar.style';
 import { Link } from 'react-router-dom';
 import { FaGripLines } from 'react-icons/fa';
 import Logo from '../../Images/logo.svg';
+import { productsSelector } from '../../Store/selectors/productsSelector';
+import { useSelector } from 'react-redux';
 
-const NavBar = ({
-  handleDisplayCart,
-  handleDisplayMenu,
-  numberOfCartItems,
-}) => {
+export const NavBar = ({ handleDisplayCart, handleDisplayMenu, products }) => {
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
+  useEffect(() => {
+    const productQuantityCounter = (products) => {
+      let sum = products.reduce((acc, obj) => {
+        return acc + obj.quantity;
+      }, 0);
+
+      setNumberOfItems(sum);
+    };
+
+    productQuantityCounter(products);
+  }, [products]);
+
   return (
     <Wrapper>
       <Content>
@@ -18,7 +30,7 @@ const NavBar = ({
         </Link>
         <ul>
           <li onClick={handleDisplayCart}>
-            Cart {numberOfCartItems > 0 && <span>{numberOfCartItems}</span>}
+            Cart {!!numberOfItems && <span>{numberOfItems}</span>}
           </li>
           <li onClick={handleDisplayMenu}>
             <FaGripLines />
@@ -35,4 +47,14 @@ NavBar.propTypes = {
   handleDisplayMenu: PropTypes.func,
 };
 
-export default NavBar;
+export const NavBarStore = ({ handleDisplayCart, handleDisplayMenu }) => {
+  const products = useSelector(productsSelector);
+
+  return (
+    <NavBar
+      handleDisplayCart={handleDisplayCart}
+      handleDisplayMenu={handleDisplayMenu}
+      products={products}
+    />
+  );
+};
